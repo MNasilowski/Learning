8b92f4ff066ebe5f8ccf7d29b5007e1b98544a38
MNasilowski <nasilowski.marcin@gmail.com>
2021-02-04 09:01:12 +0100

    Clearing

--- Fortran/saper.f
+++
@@ -1,9 +0,0 @@
-		program saper
-		implicit none
-		integer wymiar,m,x,y
-		parameter (wymiar = 10,m = 2)
-		integer Tab(0:wymiar+1,0:wymiar+1)
-		do 10 i = 1 , wymiar
-			x = rand(0)*10
-		call srand (m)
-		
